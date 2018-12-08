65f631b26b0ae457cadf70524630addb07a45e4a
Bryan Duong <bryandduong@gmail.com>
2018-12-08 13:17:55 -0800

    update config

--- server/models/user.js
+++ server/models/user.js
@@ -36,7 +36,7 @@ const UserSchema = new mongoose.Schema({
 UserSchema.methods.generateAuthToken = function() {
   const user = this;
   const access = 'auth';
-  const token = jwt.sign({_id: user._id.toHexString(), access}, 'gangnam').toString();
+  const token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
   user.tokens = user.tokens.concat([{access, token}]);
   return user.save().then(() => {
     return token;
@@ -64,7 +64,7 @@ UserSchema.statics.findByToken = function(token) {
   const User = this;
   let decoded;
   try {
-    decoded = jwt.verify(token, 'gangnam');
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch(e) {
     return Promise.reject();
   }
